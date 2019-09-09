9a1919b63af0957fcdd028de5c0e99e7db969ecd
dimaematveev <dimaematveev@gmail.com>
2019-09-09 17:58:11 +0300

    Ghj,e.

--- GraphDataStructure/GDS.CMD/Program.cs
+++ GraphDataStructure/GDS.CMD/Program.cs
@@ -10,6 +10,15 @@ namespace GDS.CMD
     {
         static void Main(string[] args)
         {
+            int[] mass = new int[32];
+            mass[5] = 1;
+            mass[13] = 1;
+            mass[14] = 1;
+            mass[15] = 1;
+            mass[16] = 11;
+            mass[21] = 1;
+            mass[22] = 1;
+            mass[23] = 1;
         }
     }
 }
